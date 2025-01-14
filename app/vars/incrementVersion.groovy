8b0858150bfdc8f28a0b9ba30e5b42efad745dad
Tsemb <Tsembenhoi@gmail.com>
2025-01-14 04:43:40 +0200

    Add groovy functions

--- app/vars/incrementVersion.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env groovy
-
-def call() {
-    sh "npm version major"
-    def packageJson = readJSON file: 'package.json'
-    def version = packageJson.version
-    env.IMAGE_NAME = "${version}-${BUILD_NUMBER}"
-}
