f5cc9cc2e9a68ba24c239078bffb473f25487331
Tsemb <Tsembenhoi@gmail.com>
2025-01-14 04:19:56 +0200

    Add groovy functions

---
+++ app/vars/buildAndPushImage.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+
+def buildAndPushImage() {
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        script {
+            sh "docker build -t dancedevops/my-node-app:${IMAGE_NAME} -f Dockerfile ."
+            sh 'echo $PASS | docker login -u $USER --password-stdin'
+            sh "docker push dancedevops/my-node-app:${IMAGE_NAME}"
+        }
+    }
+}
