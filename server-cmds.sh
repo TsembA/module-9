f37915f71c1f4e113e0300c22663df4a24d4f268
Tsemb <Tsembenhoi@gmail.com>
2025-01-28 03:40:16 +0200

    Deploy to EC2

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
-export IMAGE=$1
-docker-compose -f docker-compose.yaml up --detach
-echo "success"
+
+export IMAGE_NAME="dancedevops/my-node-app:$1"
+
+# Login to Docker registry
+echo $PASS | docker login -u $USER --password-stdin
+
+# Pull the latest image
+docker pull $IMAGE_NAME
+
+# Run docker-compose
+docker-compose up -d
