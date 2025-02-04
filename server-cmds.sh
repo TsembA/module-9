fd0ab1f34ba19e5449c9c73c148f80ece15ad01a
Tsemb <Tsembenhoi@gmail.com>
2025-02-04 23:12:04 +0200

    Configure path Dockerfile

--- server-cmds.sh
+++ server-cmds.sh
@@ -3,3 +3,5 @@ export IMAGE_NAME="dancedevops/my-node-app:$1"
 echo $PASS | docker login -u $USER --password-stdin
 docker pull $IMAGE_NAME
 docker-compose -f docker-compose.yaml up --detach
+
+
--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -8,7 +8,7 @@ def call(String IMAGE_NAME) {
 
     withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         script {
-            sh "docker build -t dancedevops/my-node-app:${IMAGE_NAME} -f Dockerfile ."
+            sh "docker build -t dancedevops/my-node-app:${IMAGE_NAME} -f Dockerfile /var/jenkins_home/workspace/my-pipe"
             sh 'echo "$PASS" | docker login --username "$USER" --password-stdin'
             sh "docker push dancedevops/my-node-app:${IMAGE_NAME}"
         }
